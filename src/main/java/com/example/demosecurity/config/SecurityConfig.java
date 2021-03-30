d7f1a1134c4c0bb5ba77ddfed16be45ff0eab8a0
choisaelim <75531480+choisaelim@users.noreply.github.com>
2021-03-30 20:42:20 +0900

    인메모리 유저 추가

--- src/main/java/com/example/demosecurity/config/SecurityConfig.java
+++ src/main/java/com/example/demosecurity/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.example.demosecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,5 +15,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().mvcMatchers("/", "/info").permitAll().mvcMatchers("admin").hasRole("ADMIN")
                 .anyRequest().authenticated().and().formLogin().and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("keesun").password("success").roles("USER").and().withUser("admin")
+                .password("success").roles("ADMIN");
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
